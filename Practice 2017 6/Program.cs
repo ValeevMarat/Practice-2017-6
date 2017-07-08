4dd366c3fe9536f3ab3dd962dfac53ce8acf9100
Marat <user@LAPTOP-7JKVIGGR>
2017-07-08 15:12:42 +0500

    Код написан и прокомментирован, присутсвует описание задания и ссылка на него. Защиты от "дурака" нет

--- Practice 2017 6/Program.cs
+++ Practice 2017 6/Program.cs
@@ -8,9 +8,57 @@ namespace Practice_2017_6
 {
     class Program
     {
+        static bool IsElBiggerOrEqualsM(double el, double m)
+        {
+            if (el == m)
+            {
+                Console.Write(el + "\nКонец последовательности, аN=M");
+                Console.ReadKey();
+                return true;
+            }
+            if (el > m)
+            {
+                Console.Write("\nКонец последовательности, аN не равно M");
+                Console.ReadKey();
+                return true;
+            }
+            Console.Write(el+" "); // Если элемент меньше М, то печатаем его и идём дальше
+            return false;
+        } // Проверка для начальных элементов
+
+        static void NumericalSequence(double a1, double a2, double a3, double m)
+        {
+            double a4 = (a1 + a2 + a3)/2;
+            if (a4 == m)
+            {
+                Console.Write(a4 + "\nКонец последовательности, аN=M");
+                return;
+            }
+            if (a4 > m)
+            {
+                Console.Write("\nКонец последовательности, аN не равно M");
+                return;
+            }
+            Console.Write(a4 + " "); // Если элемент меньше М, то печатаем его и идём дальше
+            NumericalSequence(a2, a3, a4, m);
+        } // Вычисление и вывод числовой последовательности
+
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Последовательно через пробел введите: а1, а2 и а3");
+            string[] numbers = Console.ReadLine().Split(' ');
+
+            Console.Write("Введите М: ");
+            double m = double.Parse(Console.ReadLine());
+
+            double a3 = double.Parse(numbers[2]), a2 = double.Parse(numbers[1]), a1 = double.Parse(numbers[0]);
+
+            if(IsElBiggerOrEqualsM(a1, m)) return; // Проверка первых элементов по очереди, сделано отдельно, т.к. вместе проходится по каждому условию и печатаются лишние строки
+            if(IsElBiggerOrEqualsM(a2, m)) return;
+            if(IsElBiggerOrEqualsM(a3, m)) return;
+
+            NumericalSequence(a1, a2, a3, m);
+            Console.ReadKey();
         }
     }
 }
