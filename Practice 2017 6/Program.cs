a50660b171f91557821bc18c814ce7dad59f394f
Marat <user@LAPTOP-7JKVIGGR>
2017-07-19 19:52:49 +0500

    Добавлен вывод в случае несуществования последовательности

--- Practice 2017 6/Program.cs
+++ Practice 2017 6/Program.cs
@@ -8,39 +8,41 @@ namespace Practice_2017_6
 {
     class Program
     {
-        static bool IsElBiggerOrEqualsM(double el, double m)
+        static bool IsElBiggerOrEqualsM(double el, double m, int n)
         {
             if (el == m)
             {
-                Console.Write(el + "\nКонец последовательности, аN=M\n");
+                Console.Write(el+"\nN = "+ n + "\nКонец последовательности, аN=M\n");
                 Console.ReadKey();
                 return true;
             }
             if (el > m)
             {
-                Console.WriteLine("\nКонец последовательности, аN не равно M");
+                Console.WriteLine(n == 1 // Если первый элемент больше M, значит последовательность пуста
+                    ? "пуста" + "\nN не существует"
+                    : "\nN = " + n + "\nКонец последовательности, аN не равно M");
                 Console.ReadKey();
                 return true;
             }
-            Console.Write(el+" "); // Если элемент меньше М, то печатаем его и идём дальше
+            Console.Write(el+" ");       // Если элемент меньше М, то печатаем его и идём дальше
             return false;
         }                     // Проверка для начальных элементов
 
-        static void NumericalSequence(double a1, double a2, double a3, double m)
+        static void NumericalSequence(double a1, double a2, double a3, double m, int n)
         {
             double a4 = (a1 + a2 + a3)/2;
             if (a4 == m)
             {
-                Console.Write(a4 + "\nКонец последовательности, аN=M");
+                Console.Write(a4 + "\nN = " + n + "\nКонец последовательности, аN=M");
                 return;
             }
             if (a4 > m)
             {
-                Console.Write("\nКонец последовательности, аN не равно M");
+                Console.Write("\nN = " + (n-1) + "\nКонец последовательности, аN не равно M");
                 return;
             }
             Console.Write(a4 + " "); // Если элемент меньше М, то печатаем его и идём дальше
-            NumericalSequence(a2, a3, a4, m);
+            NumericalSequence(a2, a3, a4, m, n+1);
         } // Вычисление и вывод числовой последовательности
 
         static void Main(string[] args)
@@ -49,13 +51,12 @@ namespace Practice_2017_6
             Console.Write("Введите а2: "); double a2 = Read.Double();
             Console.Write("Введите а3: "); double a3 = Read.Double();
             Console.Write("Введите М: "); double m = Read.Double();
-            
-            if(IsElBiggerOrEqualsM(a1, m)) return; // Проверка первых элементов по очереди, сделано отдельно, т.к. вместе проходится по каждому условию и печатаются лишние строки
-            if(IsElBiggerOrEqualsM(a2, m)) return;
-            if(IsElBiggerOrEqualsM(a3, m)) return;
 
-            NumericalSequence(a1, a2, a3, m);
+            Console.Write("Последоватлеьность: ");
+            if (IsElBiggerOrEqualsM(a1, m, 1) || IsElBiggerOrEqualsM(a2, m, 2) || IsElBiggerOrEqualsM(a3, m, 3)) return; // Проверка первых элементов по очереди
+
+            NumericalSequence(a1, a2, a3, m, 4);
             Console.ReadKey();
-        }                                          // Чтение переменных, вызов их сравнения с M и после вызов вычисления и вывод последовательности
+        }                                                 // Чтение переменных, вызов их сравнения с M и после вызов вычисления и вывод последовательности
     }
 }
